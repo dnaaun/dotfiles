2d87d8c7a20f312ca1bdadf3fc6d3f5aa6bcd166
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2022-05-10 16:55:55 +0300

    yay! Created a luasnip snippet for useState()

--- .config/nvim/lua/pconfig/dressing.lua
+++ .config/nvim/lua/pconfig/dressing.lua
@@ -2,8 +2,16 @@ return {
 	"stevearc/dressing.nvim",
 	config = function()
 		require("dressing").setup({
-    select = {
-      enabled = false, -- Gotta figure out some error with telescope
-    }})
+			select = {
+				enabled = true, -- Gotta figure out some error with telescope
+
+        -- Currently the builtin backend is dope, so I don't use this. But when
+        -- I do, I need this because my default settings for telescope don't
+        -- vibe well with dressing's default settings.
+				telescope = {},
+
+				backend = { "builtin" },
+			},
+		})
 	end,
 }
--- .config/nvim/lua/pconfig/luasnip.lua
+++ .config/nvim/lua/pconfig/luasnip.lua
@@ -2,6 +2,9 @@ return {
 	"L3MON4D3/LuaSnip",
 	config = function()
 		local ls = require("luasnip")
+
+		ls.config.setup({ update_events = "TextChanged,TextChangedI" })
+
 		-- some shorthands...
 		local s = ls.snippet
 		local sn = ls.snippet_node
@@ -22,14 +25,47 @@ return {
 		local types = require("luasnip.util.types")
 		local conds = require("luasnip.extras.expand_conditions")
 
+		-- (At least when reloading this file), snippets get added multipel times
+		-- sometimes, so remove all before adding them.
+		ls.cleanup()
+
+		-- Make the snippets a bit more ergonomic to write
+		local fmt = require("luasnip.extras.fmt").fmt
+
+		local same = function(index)
+			return f(function(arg)
+				print(vim.inspect(arg))
+				return ""
+			end, { index })
+		end
+
+		--- Used for useState() so far
+		--- @param str string
+		local function first_to_upper(str)
+			return (str:gsub("^%l", string.upper))
+		end
+
+		ls.add_snippets("typescriptreact", {
+			s(
+				"us",
+				fmt("const [{}, {}] = useState({})", {
+					i(1, "name"),
+					f(function(arg)
+						local state_name = arg[1][1]
+						return "set" .. first_to_upper(state_name)
+					end, { 1 }),
+					i(2),
+				})
+			),
+			s(
+				"test",
+				f(function()
+					return "UMMM"
+				end, {})
+			),
+		})
+
 		ls.snippets = {
-			-- When trying to expand a snippet, luasnip first searches the tables for
-			-- each filetype specified in 'filetype' followed by 'all'.
-			-- If ie. the filetype is 'lua.c'
-			--     - luasnip.lua
-			--     - luasnip.c
-			--     - luasnip.all
-			-- are searched in that order.
 			markdown = {
 				s(
 					"ct",
