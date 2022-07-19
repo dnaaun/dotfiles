b9e211b16a9a78fa3757e152b3ff25781c65b346
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2022-07-19 17:49:50 +0300

    Fix hopper not working nicely with manually specified treesitter parser for some filetypes
    Depends on: https://github.com/davidatsurge/nvim-treehopper/commit/b3dc491abeea81e2ab0e52dbed86b75096b5dfd5

--- .config/nvim/lua/pconfig/treehopper.lua
+++ .config/nvim/lua/pconfig/treehopper.lua
@@ -3,5 +3,6 @@ return {
 	config = function()
 		vim.keymap.set("o", "m", require("tsht").nodes, { silent = true })
 		vim.keymap.set("v", "m", require("tsht").nodes, { silent = true })
+    require("tsht").config.ft_to_parser.typescriptreact = "tsx"
 	end,
 }
