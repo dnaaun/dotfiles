59f1d85619dddc851eed3e76166eeb461195359d
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2022-12-28 21:03:49 +0300

    insert mode mapping to switch in and out of amharic mode

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -2,14 +2,14 @@ local o = vim.opt
 local g = vim.g
 
 -- Text iwidth
-o.tw=88
+o.tw = 88
 
 o.expandtab = true
 -- show existing tab with 2 spaces width
-o.tabstop=2
-o.softtabstop=2
+o.tabstop = 2
+o.softtabstop = 2
 -- when indenting with '>', use 2 spaces width
-o.shiftwidth=2
+o.shiftwidth = 2
 
 -- Enable spelling
 o.spell = true
@@ -21,3 +21,14 @@ g.vim_markdown_folding_disabled = 1
 g.vim_markdown_conceal = 0
 ---- Markdown, make preview available remotely (ie, serve on 0.0.0.0, not localhost)
 g.mkdp_open_to_the_world = 1
+
+local original_timeoutlen = vim.o.timeoutlen
+
+vim.keymap.set("i", "<C-a>", function()
+	local amharic_is_on = require("amharic").toggle_amharic()
+	if amharic_is_on and vim.o.timeoutlen < 2000 then
+		vim.o.timeoutlen = 2000
+	elseif not amharic_is_on then
+		vim.o.timeoutlen = original_timeoutlen
+	end
+end, { buffer = true })
