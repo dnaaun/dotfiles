6f6bb35949e3e8575df87085627cd06c0dce5408
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-04-27 03:22:37 +0300

    changes that were probably unecssary when trying to get tailwindcss to work

--- .config/nvim/lua/pconfig/lsp_config/tailwindcss.lua
+++ .config/nvim/lua/pconfig/lsp_config/tailwindcss.lua
@@ -2,13 +2,23 @@ return {
 	filetypes = vim.list_extend({ "rust" }, require("consts").javascripty_filetypes),
 	init_options = {
 		userLanguages = {
-			rust = "rust",
+			rust = "html",
+			["*.rs"] = "html",
 		},
 	},
 	settings = {
 		tailwindCSS = {
 			includeLanguages = {
 				rust = "html",
+				["*.rs"] = "html",
+			},
+			experimental = {
+				classRegex = {
+					-- Start autocompletion when `class("...")`
+					-- This is useful for Leptos/sycamore.
+					{ [[\.class\(([^\)]*)\)]], '"([^"]*)"' },
+					{ [[\.classes\(([^\)]*)\)]], '"([^"]*)"' },
+				},
 			},
 			emmetCompletions = true,
 			classAttributes = { "class", "className", "classList", "ngClass" },
