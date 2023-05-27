5a8359bfb8c12df526f3e8e6292472f2e0b24692
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-05-27 16:17:25 +0300

    remove stuff that I think I added for firenvim

--- .config/nvim/lua/pconfig/auto_session.lua
+++ .config/nvim/lua/pconfig/auto_session.lua
@@ -7,22 +7,6 @@ augroup END
 	false
 )
 
-local g = vim.api.nvim_create_augroup("SessionLoad", {})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	group = g,
-	nested = true,
-	callback = function()
-		if not vim.g.started_by_firenvim and vim.fn.argc() <= 0 then
-			require("auto-session").RestoreSession()
-    end
-    if vim.g.started_by_firenvim then
-      vim.o.guifont="Fira_Code:h24"
-      vim.opt_local.filetype = "markdown"
-		end
-		return true
-	end,
-})
 
 return {
 	"rmagatti/auto-session",
