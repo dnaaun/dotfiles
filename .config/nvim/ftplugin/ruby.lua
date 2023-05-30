24e14cd0000853b6704bf98ec15e5b059f4b083b
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-05-30 10:22:39 +0300

    fix: nvim-surround, lazy loading, ruby files

--- .config/nvim/ftplugin/ruby.lua
+++ .config/nvim/ftplugin/ruby.lua
@@ -10,6 +10,13 @@ vim.api.nvim_create_autocmd({ "BufAdd", "BufCreate", "BufEnter" }, {
 	end,
 })
 
+-- I have nvim-surround to load in InsertEnter in my lazy.nvim config,
+-- which means, potentially, that this file (ie ftplubin/ruby.lua) is loaded before 
+-- nvim-surround is loaded. So, I'm going to load it here, too.
+-- The fact that I have `module = true` in my lazy.nvim config for nvim-surround
+-- probably helps here too.
+require("pconfig.surround").config()
+
 -- Following the blueprint: https://github.com/kylechui/nvim-surround/discussions/53#discussioncomment-3341113
 require("nvim-surround").buffer_setup({
 	surrounds = {
