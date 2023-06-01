a5b2214bc3492a233e0c2c304a54fa7dbebc09be
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-06-01 12:52:00 +0300

    .

--- .config/nvim/lua/pconfig/telescope_file_browser.lua
+++ .config/nvim/lua/pconfig/telescope_file_browser.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope-file-browser.nvim",
   keys = require("pconfig.telescope").keys,
+  dependencies = { "nvim-telescope/telescope.nvim" },
 	config = function()
 		require("telescope").load_extension("file_browser")
 	end,
