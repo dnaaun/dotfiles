8116986a83c98631ccc20ccc8a89a7c1e4e0be02
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-06-16 16:10:22 +0300

    .

--- .config/nvim/lua/pconfig/telescope_fzf_native.lua
+++ .config/nvim/lua/pconfig/telescope_fzf_native.lua
@@ -1,6 +1,8 @@
 return {
-	"nvim-telescope/telescope-fzf-native.nvim",
+  "nvim-telescope/telescope-fzf-native.nvim",
   keys = require("pconfig.telescope").keys,
+  event = require("pconfig.telescope").event,
+  module = require("pconfig.telescope").module,
 	build = "make",
 	config = function()
 		require("telescope").load_extension("fzf")
