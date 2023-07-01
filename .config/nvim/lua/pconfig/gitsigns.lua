c1e262386323f274cc428afd627dc1714686ffd2
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2023-07-01 22:45:14 +0300

    fix gitsigns bindings sometimes not working

--- .config/nvim/lua/pconfig/gitsigns.lua
+++ .config/nvim/lua/pconfig/gitsigns.lua
@@ -1,51 +1,50 @@
+require("which-key").register({
+	["]h"] = { function() require("gitsigns").next_hunk() end, "next hunk" },
+	["[h"] = { function() require("gitsigns").prev_hunk() end, "previous hunk" },
+	["<leader>g"] = {
+		w = {  function() require("gitsigns").stage_hunk () end, "stage hunk" },
+		u = { function() require("gitsigns").undo_stage_hunk() end, "undo stage hunk" },
+		r = {
+			function()
+				require("gitsigns").reset_hunk()
+			end,
+			"reset hunk",
+		},
+		p = { function() require("gitsigns").preview_hunk() end, "preview hunk" },
+		b = { function() require("gitsigns").blame_line() end, "blame line" },
+		W = { function() require("gitsigns").stage_buffer() end, "stage buffer" },
+	},
+}, { mode = "n" })
+
+require("which-key").register({
+	["<leader>g"] = {
+		w = {
+			function()
+				require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end,
+			"stage_hunk",
+		},
+		u = {
+			function()
+				require("gitsigns").undo_stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end,
+			"stage hunk",
+		},
+		r = {
+			function()
+				require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end,
+			"reset_hunk",
+		},
+	},
+}, { mode = "v" })
+
 return {
 	"lewis6991/gitsigns.nvim",
 	requires = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
-	keys = {
-    "<leader>gw",
-		"<leader>gu",
-		"<leader>gr",
-		"<leader>gp",
-		"<leader>gb",
-		"<leader>gW",
-		{
-			"ih",
-			mode = "o",
-		},
-		{
-			"ih",
-			mode = "x",
-		},
-	},
+	event = "VeryLazy",
+  module = "gitsigns",
 	config = function()
-		require("gitsigns").setup({
-			keymaps = {
-				noremap = true,
-
-				["n ]h"] = {
-					expr = true,
-					"&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'",
-				},
-				["n [h"] = {
-					expr = true,
-					"&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
-				},
-
-				["n <leader>gw"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-				["v <leader>gw"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-				["n <leader>gu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-				["v <leader>gu"] = '<cmd>lua require"gitsigns".undo_stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-				["n <leader>gr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-				["v <leader>gr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-				["n <leader>gp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-				["n <leader>gb"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-				["n <leader>gW"] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-
-				-- Text objects
-				["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-				["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-			},
-		})
+		require("gitsigns").setup({})
 	end,
 }
