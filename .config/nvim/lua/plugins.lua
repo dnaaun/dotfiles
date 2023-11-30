3e6b21888458190a0267d63cc40c79c501890106
dnauun <52462475+dnaaun@users.noreply.github.com>
2023-11-30 11:29:54 +0300

    .

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,7 +33,7 @@ return {
 
 	require("pconfig.lsp_config"),
 	require("pconfig.lsp_extensions"),
-	-- require("pconfig.rust_tools"),
+	require("pconfig.rust_tools"),
 	-- require("pconfig.lsp_inlayhints"),
 
 	-- cmp-related, might be useful when using neovim in pager mode.
