d4ea5af08f7232ce168498c488fdbadb7a29e0c4
dnauun <davidtofu2018@gmail.com>
2024-03-12 21:20:20 +0300

    .

--- .config/nvim/lua/pconfig/lsp_config/sorbet.lua
+++ .config/nvim/lua/pconfig/lsp_config/sorbet.lua
@@ -1,7 +1,10 @@
 local root_pattern = require("lspconfig").util.root_pattern;
 
 return {
-	cmd = { "sorbet", "tc", "--lsp", "--disable-watchman" },
+	cmd = { "srb", "tc", "--lsp",
+  -- Not sure I need this on linux.
+  "--disable-watchman"
+},
 	filetypes = { "ruby" },
 	root_dir = root_pattern("Gemfile", ".git"),
 }
